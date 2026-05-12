26f6d85c720ccb0a68f01ee32c56285d76d05c3e
bimo-endika <bimo3046@gmail.com>
2026-05-12 12:07:06 +0800

    Add files via upload

---
+++ looping.php
@@ -0,0 +1,73 @@
+<center>
+<h1>While Loop</h1>
+<?php
+//while loop
+$jumlah_barang = 1;
+while ($jumlah_barang < 6) {
+    echo "nomor saat ini: $jumlah_barang<br>";
+    $jumlah_barang++;
+}
+echo "<br>";
+?>
+<h1>Break</h1>
+<?php
+//break 
+$i = 1;
+while ($i <= 10) {
+    if ($i == 5) break;
+    echo "Angka: $i<br>";
+    $i++;
+}
+echo "<br>";
+?>
+<h1>Continue</h1>
+<?php
+//continue
+$i = 0;
+while ($i <= 10) {
+    $i++;
+    if ($i == 5) continue;
+    echo "Angka: $i<br>";
+}
+echo "<br>";
+?>
+<h1>Alternative Syntax</h1>
+<?php
+//alternative syntax
+$i = 1;
+while ($i <= 5):
+    echo "Angka: $i<br>";
+    $i++;
+endwhile;
+
+echo "<br>";
+
+$i = 0;
+while ($i < 100) {
+    $i+=10;
+    echo 'perhitingan ke: ' . $i . '<br>';
+}
+
+echo '<br>';
+?>
+<h1>Do While Loop</h1>
+<?php
+//dowhile
+$angka = 0;
+do {
+    echo "Angka: $angka<br>";
+    $angka++;
+} while ($angka < 5);
+
+echo "<br>";
+
+?>
+<?php
+//looping for
+for($hitung =0; $hitung <10; $hitung--){
+    if ($hitung == -101) break;
+    echo "Perhitungan ke:" . $hitung . "<br>";
+}
+
+?>
+</center>
